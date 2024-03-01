c4440ab0bece4f3b242ae280240840c1d6228133
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-03-01 15:53:29 +0530

    Commit

---
+++ Backend/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace Backend.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public ICollection<Movie> Movies { get; set;}
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
+ 
