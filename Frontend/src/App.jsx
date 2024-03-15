d332b110430b85794cd52c5dac73abd22230f09a
rohankamathzapcg <rohan.kamath@zapcg.com>
2024-03-15 10:16:34 +0530

    Morning Session

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,21 +5,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigationbar from '../src/Components/Navigationbar';
 import Footer from '../src/Components/Footer';
 import HomePage from '../src/Components/HomePage';
-import Regions from '../src/Components/Regions';
-import Difficulty from '../src/Components/Difficulty';
-import Walk from '../src/Components/Walk';
+
+import Regions from '../src/Pages/Regions';
+import Difficulty from '../src/Pages/Difficulty';
+import Walk from '../src/Pages/Walk';
+import CreateRegions from '../src/Pages/CreateRegions'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Navigationbar/>
+        <Navigationbar />
         <Routes>
 
-          <Route path='/' element={<HomePage/>} />
-          <Route path='/regions' element={<Regions/>} />
-          <Route path='/difficulty' element={<Difficulty/>} />
-          <Route path='/walks' element={<Walk/>} />
+          <Route path='/' element={<HomePage />} />
+          
+          <Route path='/regions' element={<Regions />}>
+            <Route path='/regions/add_regions' element={<CreateRegions />}/>
+          </Route>
+
+          <Route path='/difficulty' element={<Difficulty />} />
+          <Route path='/walks' element={<Walk />} />
 
         </Routes>
         <Footer />
